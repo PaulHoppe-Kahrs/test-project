26cdc8ed700807769253846fae48de8ca8bc79fe
Paul Hoppe <deine-email@beispiel.de>
2026-09-15 17:14:49 +0200

    Formular in php

---
+++ formularphp/form.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Hello World</title>
+</head>
+<body>
+  <form action="#" method="post">
+
+    <div class="input-container">
+      <label for="input-username">Username</label>
+      <input type="text" name="username"id="input-username">
+    </div>
+
+    <div class="input-container">
+      <label for="input-EMAIL">Number</label>
+      <input type="text" name="number"id="input-number">
+    </div>
+
+    <div class="input-container">
+      <label for="input-EMAIL">Email</label>
+      <input type="EMAIL" name="EMAIL"id="input-EMAIL">
+    </div>
+
+    <div class="input-container">
+      <label for="input-password">Password</label>
+      <input type="password" name="password"id="input-password">
+    </div>
+
+    <input type="value ="Send">
+  </form>
+</body>
+</html>
